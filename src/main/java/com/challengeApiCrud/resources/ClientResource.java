d2f5f97362da3885813291edc9b50b972308c1f4
Luis Almeida <lferreira646@gmail.com>
2023-01-08 18:15:21 -0300

    resource by id

--- src/main/java/com/challengeApiCrud/resources/ClientResource.java
+++ src/main/java/com/challengeApiCrud/resources/ClientResource.java
@@ -1,11 +1,22 @@
 package com.challengeApiCrud.resources;
 
 
+import com.challengeApiCrud.dto.ClientDTO;
+import com.challengeApiCrud.services.ClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "/client")
 public class ClientResource {
 
+    ClientService clientService;
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = clientService.findById(id);
+        return  ResponseEntity.ok().body(dto);
+    }
 }
--- src/main/java/com/challengeApiCrud/services/ClientService.java
+++ src/main/java/com/challengeApiCrud/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.challengeApiCrud.services;
 
+import com.challengeApiCrud.dto.ClientDTO;
 import com.challengeApiCrud.entities.Client;
 import com.challengeApiCrud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -13,7 +15,10 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    List<Client> findById() {
-        
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> obj = clientRepository.findById(id);
+        Client entity = obj.get();
+        return new ClientDTO(entity);
     }
 }
